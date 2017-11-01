9e63c2b23a0d68aead322d24d0b34a361a34ea38
Aleksandr Opachev <alexs6@mail.ru>
2017-11-01 11:12:21 +0400

    * Исправлено удвоение иконки скачивания на некоторых страницах СамИздата. * Убраны неиспользуемые переменные. * Убрано ненужное двоение кода.

--- samlib.js
+++ samlib.js
@@ -4,7 +4,7 @@ function samtools_add_buttons()
 {
     var head = document.getElementsByTagName('h2')[0];
     var links = document.getElementsByTagName('a');
-    var commentPatern = /\/comment\//i, bookPatern = /другие произведения/i, groupPatern = /.shtml/, patern = /<b>/i, paternFont = /<font/i;
+    var commentPatern = /\/comment\//i, bookPatern = /другие произведения/i, groupPatern = /.shtml/;
     var url = 'http://vagrand.point.od.ua/download_book/?source_url';
     if (commentPatern.test(window.location)){
         var link_c = '<a href="' + url + '=http://samlib.ru' + head.firstElementChild.getAttribute('href') + '" title="Скачать произведение" target="_blank">' + '<img src="' + chrome.extension.getURL('images/download.png') + '" border="0" style="vertical-align: middle;">' + '</a>&nbsp;';
@@ -12,18 +12,6 @@ function samtools_add_buttons()
     } else if (bookPatern.test(links[0].innerHTML)){
         var link = '<a href="' + url + '=' + window.location + '" title="Скачать произведение" target="_blank">'+ '<img src="' + chrome.extension.getURL('images/download.png') + '" border="0" style="vertical-align: middle;">'+ '</a>&nbsp;';
         head.insertAdjacentHTML('afterBegin', link);
-    } else if (groupPatern.test(window.location)){
-        var lis = document.getElementsByTagName('li');
-        for (var index in lis){
-            if (lis[index].parentElement.tagName == "DT"){
-                for (var i=0;i<lis[index].children.length;i++){
-                    if (lis[index].children[i].tagName == 'A'){
-                        var anchor = lis[index].children[i].href;
-                        lis[index].firstElementChild.insertAdjacentHTML('afterbegin', '<a href="'+url+'='+anchor+'" title="Скачать произведение" target="_blank"><img src="'+chrome.extension.getURL('images/download.png')+'" border="0" style="vertical-align: middle;"></a>');
-                    };
-                };
-            };
-        };
     } else {
         var lis = document.getElementsByTagName('li');
         for (var index in lis){
@@ -31,7 +19,7 @@ function samtools_add_buttons()
                 for (var i=0;i<lis[index].children.length;i++){
                     if (lis[index].children[i].tagName == 'A'){
                         var anchor = lis[index].children[i].href;
-                        if (anchor != ""){
+                        if (groupPatern.test(anchor)){
                             lis[index].firstElementChild.insertAdjacentHTML('afterbegin', '<a href="'+url+'='+anchor+'" title="Скачать произведение" target="_blank"><img src="'+chrome.extension.getURL('images/download.png')+'" border="0" style="vertical-align: middle;"></a>');
                         };
                         
