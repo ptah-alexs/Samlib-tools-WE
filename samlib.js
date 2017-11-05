98dbc55ecc6e4d5c9eab07a2d279159ed7da31b3
Aleksandr Opachev <alexs6@mail.ru>
2017-11-05 15:42:32 +0400

    * Ссылка для скачивания теперь учитывает текущий домен (samlib, budclub, zhurnal.lib), ранее использовался samlib независимо от реального домена. * Обновлен README. * Переименованы переменные паттернов.

--- samlib.js
+++ samlib.js
@@ -3,14 +3,18 @@ samtools_add_buttons();
 function samtools_add_buttons()
 {
     var head = document.getElementsByTagName('h2')[0];
-    var links = document.getElementsByTagName('a');
-    var commentPatern = /\/comment\//i, bookPatern = /другие произведения/i, groupPatern = /.shtml/;
+    var links = document.getElementsByTagName('a')[0];
+    var link = "";
+    var comment_patern = /\/comment\//i;
+    var book_patern = /другие произведения/i;
+    var group_patern = /.shtml/;
     var url = 'http://vagrand.point.od.ua/download_book/?source_url';
-    if (commentPatern.test(window.location)){
-        var link_c = '<a href="' + url + '=http://samlib.ru' + head.firstElementChild.getAttribute('href') + '" title="Скачать произведение" target="_blank">' + '<img src="' + chrome.extension.getURL('images/download.png') + '" border="0" style="vertical-align: middle;">' + '</a>&nbsp;';
-        head.insertAdjacentHTML('afterBegin', link_c);
-    } else if (bookPatern.test(links[0].innerHTML)){
-        var link = '<a href="' + url + '=' + window.location + '" title="Скачать произведение" target="_blank">'+ '<img src="' + chrome.extension.getURL('images/download.png') + '" border="0" style="vertical-align: middle;">'+ '</a>&nbsp;';
+    if (comment_patern.test(window.location)){
+        var parts = window.location.toString().split('/');
+        link = '<a href="' + url + '=http://' + parts[2] + head.firstElementChild.getAttribute('href') + '" title="Скачать произведение" target="_blank">' + '<img src="' + chrome.extension.getURL('images/download.png') + '" border="0" style="vertical-align: middle;">' + '</a>&nbsp;';
+        head.insertAdjacentHTML('afterBegin', link);
+    } else if (book_patern.test(links.innerHTML)){
+        link = '<a href="' + url + '=' + window.location + '" title="Скачать произведение" target="_blank">'+ '<img src="' + chrome.extension.getURL('images/download.png') + '" border="0" style="vertical-align: middle;">'+ '</a>&nbsp;';
         head.insertAdjacentHTML('afterBegin', link);
     } else {
         var lis = document.getElementsByTagName('li');
@@ -19,10 +23,9 @@ function samtools_add_buttons()
                 for (var i=0;i<lis[index].children.length;i++){
                     if (lis[index].children[i].tagName == 'A'){
                         var anchor = lis[index].children[i].href;
-                        if (groupPatern.test(anchor)){
-                            lis[index].firstElementChild.insertAdjacentHTML('afterbegin', '<a href="'+url+'='+anchor+'" title="Скачать произведение" target="_blank"><img src="'+chrome.extension.getURL('images/download.png')+'" border="0" style="vertical-align: middle;"></a>');
+                        if (group_patern.test(anchor)){
+                            lis[index].firstElementChild.insertAdjacentHTML('afterbegin', '<a href="'+url+'='+anchor+'" title="Скачать произведение" target="_blank"><img src="'+chrome.extension.getURL('images/download.png')+'" border="0" style="vertical-align: middle;"></a>&nbsp;');
                         };
-                        
                     };
                 };
             };
